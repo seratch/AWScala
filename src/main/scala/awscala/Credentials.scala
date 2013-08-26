d32a42b9d5e7faacc0d2f4dd4a721cbdd24569e8
Kazuhiro Sera <seratch@gmail.com>
2013-08-27 07:51:06 +0900

    Let Credentials.defaultEnv throw Exception when env values are not found

--- src/main/scala/awscala/Credentials.scala
+++ src/main/scala/awscala/Credentials.scala
@@ -2,10 +2,19 @@ package awscala
 
 object Credentials {
 
-  def defaultEnv: Credentials = new Credentials(
-    accessKeyId = System.getenv("AWS_ACCESS_KEY_ID"),
-    secretAccessKey = System.getenv("AWS_SECRET_ACCESS_KEY")
-  )
+  val AWS_ACCESS_KEY_ID = "AWS_ACCESS_KEY_ID"
+  val AWS_SECRET_ACCESS_KEY = "AWS_SECRET_ACCESS_KEY"
+
+  def defaultEnv: Credentials = {
+    new Credentials(
+      accessKeyId = Option(System.getenv(AWS_ACCESS_KEY_ID)).getOrElse {
+        throw new IllegalStateException(s"'${AWS_ACCESS_KEY_ID}' env value was not found!")
+      },
+      secretAccessKey = Option(System.getenv(AWS_SECRET_ACCESS_KEY)).getOrElse {
+        throw new IllegalStateException(s"'${AWS_SECRET_ACCESS_KEY}' env value was not found!")
+      }
+    )
+  }
 
 }
 
