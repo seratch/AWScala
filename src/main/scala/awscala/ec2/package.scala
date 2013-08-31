eea33eb7f57cc89382ff9e5c12f07fc7f66aac1b
taisukeoe <oeuia.t@gmail.com>
2013-08-31 09:21:17 +0900

    added getX methods with wraping options / reboot method in Instance

--- src/main/scala/awscala/ec2/package.scala
+++ src/main/scala/awscala/ec2/package.scala
@@ -5,7 +5,9 @@ import com.amazonaws.services.{ ec2 => aws }
 
 package object ec2 {
   def wrapOption[A](value: A): Option[A] = if (null != value) Some(value) else None
-
   def convOption[A, B](value: A)(conv: A => B): Option[B] = if (null != value) Some(conv(value)) else None
+
+  type InstanceType = com.amazonaws.services.ec2.model.InstanceType
+
 }
 
