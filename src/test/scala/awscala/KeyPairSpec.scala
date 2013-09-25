897e01139dd12997b1e6130352ea3ca529167ff5
Chris Birchall <c-birchall@m3.com>
2013-09-25 19:22:27 +0900

    Implement some of the methods inherited from aws.model.KeyPair.
    It's easy to accidentally call these methods and end up dealing with
    nulls.
    
    Actually, maybe KeyPair doesn't need to extend aws.model.KeyPair anyway?

---
+++ src/test/scala/awscala/KeyPairSpec.scala
@@ -0,0 +1,41 @@
+package awscala
+
+import awscala._, ec2._
+
+import org.slf4j._
+import org.scalatest._
+import org.scalatest.matchers._
+
+import java.io._
+
+class KeyPairSpec extends FlatSpec with ShouldMatchers {
+
+  behavior of "KeyPair"
+
+  val log = LoggerFactory.getLogger(this.getClass)
+
+  it should "properly implement the inherited Java methods" in {
+    implicit val ec2 = EC2.at(Region.Tokyo)
+
+    val keyPairName = s"awscala-unit-test-keypair-${System.currentTimeMillis}"
+    val keyPair = ec2.createKeyPair(keyPairName)
+
+    try {
+      keyPair.name should be(keyPairName)
+      keyPair.fingerprint.size should be > (0)
+      // Newly created key pair has material (i.e. private key)
+      keyPair.material should be('defined)
+      keyPair.material.get.size should be > (0)
+
+      keyPair.getKeyName should equal(keyPair.name)
+      keyPair.getKeyFingerprint should equal(keyPair.fingerprint)
+      keyPair.getKeyMaterial should equal(keyPair.material.get)
+
+      // Key pair without private key info defined
+      KeyPair("my-key", "fingerprint", None).getKeyMaterial should be(null)
+
+    } finally {
+      ec2.deleteKeyPair(keyPairName)
+    }
+  }
+}
