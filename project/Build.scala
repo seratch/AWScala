600c16a085d60b73db0ac6f5dcf4f677890dd30d
Kazuhiro Sera <seratch@gmail.com>
2014-01-16 19:38:31 +0900

    0.1.3-SNAPSHOT

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ object AwscalaProject extends Build {
   lazy val mainSettings: Seq[Project.Setting[_]] = Defaults.defaultSettings ++ Seq(
     organization := "com.github.seratch",
     name := "awscala",
-    version := "0.1.2",
+    version := "0.1.3-SNAPSHOT",
     scalaVersion := "2.10.3",
     crossScalaVersions := Seq("2.10.0"),
     publishTo <<= version { (v: String) => 
