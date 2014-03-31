d8b36ffbcb32846826df2d80d0037ae3562842cf
Kazuhiro Sera <seratch@gmail.com>
2014-03-31 11:26:07 +0900

    version 0.2.1

--- project/Build.scala
+++ project/Build.scala
@@ -5,11 +5,11 @@ object AwscalaProject extends Build {
 
   lazy val root = Project("root", file("."), settings = mainSettings)
 
-  lazy val mainSettings: Seq[Project.Setting[_]] = Defaults.defaultSettings ++ Seq(
+  lazy val mainSettings: Seq[Project.Setting[_]] = Seq(
     organization := "com.github.seratch",
     name := "awscala",
-    version := "0.2.0",
-    scalaVersion := "2.10.3",
+    version := "0.2.1",
+    scalaVersion := "2.10.4",
     crossScalaVersions := Seq("2.10.0"),
     publishTo <<= version { (v: String) => 
       val nexus = "https://oss.sonatype.org/"
@@ -19,12 +19,12 @@ object AwscalaProject extends Build {
     publishMavenStyle := true,
     resolvers += "spray repo" at "http://repo.spray.io",
     libraryDependencies ++= Seq(
-      "com.amazonaws"    %  "aws-java-sdk"    % "1.7.1",
+      "com.amazonaws"    %  "aws-java-sdk"    % "1.7.2",
       "joda-time"        %  "joda-time"       % "2.3",
-      "org.joda"         %  "joda-convert"    % "1.5",
+      "org.joda"         %  "joda-convert"    % "1.6",
       "com.decodified"   %% "scala-ssh"       % "0.6.4"  % "provided",
       "org.bouncycastle" %  "bcprov-jdk16"    % "1.46"   % "provided",
-      "ch.qos.logback"   %  "logback-classic" % "1.0.13" % "test",
+      "ch.qos.logback"   %  "logback-classic" % "1.1.1"  % "test",
       "org.scalatest"    %% "scalatest"       % "2.0"    % "test"
     ),
     sbtPlugin := false,
