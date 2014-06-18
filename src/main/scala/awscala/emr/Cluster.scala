6bcb77ef59d93f66a1d0daedab730a7db3f5ff5b
Derrick Burns <derrickrburns@gmail.com>
2014-06-18 16:41:57 -0700

    Formatting.

--- src/main/scala/awscala/emr/Cluster.scala
+++ src/main/scala/awscala/emr/Cluster.scala
@@ -5,7 +5,7 @@ import com.amazonaws.services.{ elasticmapreduce => aws }
 case class Cluster(id: String) {
   def name(implicit emr: EMR): String = emr.getClusterName(id)
   def state(implicit emr: EMR): String = emr.getClusterState(id)
-  def bootstrapActions(implicit emr: EMR) : Seq[aws.model.Command] = emr.bootstrapActions(Some(id))
-  def steps(implicit emr: EMR) : Seq[aws.model.StepSummary] = emr.steps(Some(id))
+  def bootstrapActions(implicit emr: EMR): Seq[aws.model.Command] = emr.bootstrapActions(Some(id))
+  def steps(implicit emr: EMR): Seq[aws.model.StepSummary] = emr.steps(Some(id))
   def terminate(implicit emr: EMR) = emr.terminateCluster(id)
 }
