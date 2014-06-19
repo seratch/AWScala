643f88326cfa0542db8a40cc48b89e915a3afb70
Derrick Burns <derrickrburns@gmail.com>
2014-06-19 10:34:36 -0700

    Add status method on new cluster object that returns the latest status.

--- src/main/scala/awscala/emr/Cluster.scala
+++ src/main/scala/awscala/emr/Cluster.scala
@@ -21,17 +21,21 @@ case class Cluster(in: aws.model.Cluster) extends aws.model.Cluster {
 
   def bootstrapActions(implicit emr: EMR): Seq[aws.model.Command] = {
     cachedBootstrapActions match {
-      case None =>
-        cachedBootstrapActions = Some(emr.bootstrapActions(Some(getId))); cachedBootstrapActions.get
+      case None => cachedBootstrapActions = Some(emr.bootstrapActions(Some(getId))); cachedBootstrapActions.get
       case Some(e) => e
     }
   }
 
   def stepSummaries(implicit emr: EMR): Seq[aws.model.StepSummary] = {
     cachedStepSummares match {
-      case None =>
-        cachedStepSummares = Some(emr.stepSummaries(Some(getId))); cachedStepSummares.get
+      case None => cachedStepSummares = Some(emr.stepSummaries(Some(getId))); cachedStepSummares.get
       case Some(e) => e
     }
   }
+   
+  def status(implicit emr: EMR) : aws.model.ClusterStatus = {
+    val describeClusterRequest = new aws.model.DescribeClusterRequest().withClusterId(getId)
+    emr.describeCluster(describeClusterRequest).getCluster().getStatus()
+  }
+  
 }
