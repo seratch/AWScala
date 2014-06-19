9db2e32c5b394a4e6702243fcfffe176638c596b
Derrick Burns <derrickrburns@gmail.com>
2014-06-18 18:18:31 -0700

    Interface change for EMR package.  Need to bump version.

--- src/main/scala/awscala/emr/Cluster.scala
+++ src/main/scala/awscala/emr/Cluster.scala
@@ -6,6 +6,6 @@ case class Cluster(id: String) {
   def name(implicit emr: EMR): String = emr.getClusterName(id)
   def state(implicit emr: EMR): String = emr.getClusterState(id)
   def bootstrapActions(implicit emr: EMR): Seq[aws.model.Command] = emr.bootstrapActions(Some(id))
-  def steps(implicit emr: EMR): Seq[aws.model.StepSummary] = emr.steps(Some(id))
+  def stepSummaries(implicit emr: EMR): Seq[aws.model.StepSummary] = emr.stepSummaries(Some(id))
   def terminate(implicit emr: EMR) = emr.terminateCluster(id)
 }
