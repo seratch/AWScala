fe4b32c73df1f816e21a302a5daca4fb77a77faa
Kazuhiro Sera <seratch@gmail.com>
2014-07-29 23:34:24 +0900

    Bump AWS SDK 1.7 to 1.8, joda-time 2.3 to 2.4
     - Fix awscala.s3.PutObjectResult API deprecation

--- project/Build.scala
+++ project/Build.scala
@@ -8,9 +8,9 @@ object AwscalaProject extends Build {
   lazy val mainSettings = Seq(
     organization := "com.github.seratch",
     name := "awscala",
-    version := "0.2.5",
-    scalaVersion := "2.10.4",
-    crossScalaVersions := Seq("2.11.1", "2.10.4"),
+    version := "0.3.0-SNAPSHOT",
+    scalaVersion := "2.11.2",
+    crossScalaVersions := Seq("2.11.2", "2.10.4"),
     publishTo <<= version { (v: String) => 
       val nexus = "https://oss.sonatype.org/"
       if (v.trim.endsWith("SNAPSHOT")) Some("snapshots" at nexus + "content/repositories/snapshots")
@@ -19,8 +19,8 @@ object AwscalaProject extends Build {
     publishMavenStyle := true,
     resolvers += "spray repo" at "http://repo.spray.io",
     libraryDependencies ++= Seq(
-      "com.amazonaws"    %  "aws-java-sdk"    % "1.7.12",
-      "joda-time"        %  "joda-time"       % "2.3",
+      "com.amazonaws"    %  "aws-java-sdk"    % "1.8.6",
+      "joda-time"        %  "joda-time"       % "2.4",
       "org.joda"         %  "joda-convert"    % "1.6",
       "com.decodified"   %% "scala-ssh"       % "0.6.4"  % "provided",
       "org.bouncycastle" %  "bcprov-jdk16"    % "1.46"   % "provided",
