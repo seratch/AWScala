66e8f6a2ec077dc762b1c3069a0c0cf5be2d5e20
Kazuhiro Sera <seratch@gmail.com>
2014-12-28 09:22:19 +0900

    0.4.3

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ object AwscalaProject extends Build {
   lazy val mainSettings = Seq(
     organization := "com.github.seratch",
     name := "awscala",
-    version := "0.4.2",
+    version := "0.4.3",
     scalaVersion := "2.11.4",
     crossScalaVersions := Seq("2.11.4", "2.10.4"),
     publishTo <<= version { (v: String) => 
@@ -19,13 +19,13 @@ object AwscalaProject extends Build {
     publishMavenStyle := true,
     resolvers += "spray repo" at "http://repo.spray.io",
     libraryDependencies ++= Seq(
-      "com.amazonaws"    %  "aws-java-sdk"    % "1.9.8",
-      "joda-time"        %  "joda-time"       % "2.5",
+      "com.amazonaws"    %  "aws-java-sdk"    % "1.9.13",
+      "joda-time"        %  "joda-time"       % "2.6",
       "org.joda"         %  "joda-convert"    % "1.7",
       "com.decodified"   %% "scala-ssh"       % "0.7.0"  % "provided",
       "org.bouncycastle" %  "bcprov-jdk16"    % "1.46"   % "provided",
       "ch.qos.logback"   %  "logback-classic" % "1.1.2"  % "test",
-      "org.scalatest"    %% "scalatest"       % "2.2.2"  % "test"
+      "org.scalatest"    %% "scalatest"       % "2.2.3"  % "test"
     ),
     sbtPlugin := false,
     transitiveClassifiers in Global := Seq(Artifact.SourceClassifier),
