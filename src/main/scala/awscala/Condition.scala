e923eb3cbd6342d137f252e044cb73c266aae4c9
Kazuhiro Sera <seratch@gmail.com>
2015-06-16 03:15:46 +0900

    Fix #83 Make Condition a Case Class for consistency

--- src/main/scala/awscala/Condition.scala
+++ src/main/scala/awscala/Condition.scala
@@ -3,10 +3,10 @@ package awscala
 import scala.collection.JavaConverters._
 import com.amazonaws.auth.{ policy => aws }
 
-class Condition(val key: String, val typeName: String, values: Seq[String]) extends aws.Condition {
+case class Condition(key: String, typeName: String, conditionValues: Seq[String]) extends aws.Condition {
   setConditionKey(key)
   setType(typeName)
-  setValues(values.asJava)
+  setValues(conditionValues.asJava)
 
   def specifiedValues: Seq[String] = getValues.asScala
 }
