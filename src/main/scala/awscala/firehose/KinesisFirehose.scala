42ff8008f13797cbf623980f1bf008c21692e918
REPORTY\gilm <gil@reporty.com>
2018-02-26 16:27:13 +0200

    added bulk messages to firehose

--- src/main/scala/awscala/firehose/KinesisFirehose.scala
+++ src/main/scala/awscala/firehose/KinesisFirehose.scala
@@ -1,11 +1,9 @@
 package awscala.firehose
 
-
 import java.nio.ByteBuffer
 import java.nio.charset.StandardCharsets
-
 import com.amazonaws.regions.{Region, Regions}
-import com.amazonaws.services.kinesisfirehose.model.{PutRecordRequest, PutRecordResult, Record}
+import com.amazonaws.services.kinesisfirehose.model.{PutRecordRequest, PutRecordBatchRequest, PutRecordResult, PutRecordBatchResult, Record}
 import com.amazonaws.services.kinesisfirehose.AmazonKinesisFirehoseAsyncClient
 import com.amazonaws.services.{ kinesisfirehose => aws }
 import awscala._
@@ -14,6 +12,10 @@ import scala.collection.JavaConverters._
 
 object KinesisFirehose 
 {
+    // NOTE:: GM - Each PutRecordBatch request supports up to 500 records.
+    // Each record in the request can be as large as 1,000 KB (before 64-bit encoding), up to a limit of 4 MB for the entire request. These limits cannot be changed.
+    val PUT_RECORD_BATCH_LIMIT = 500
+    
     def apply(credentials : Credentials)(implicit region: Region) : KinesisFirehose  =
     {
         new KinesisFirehoseAsyncClient(BasicCredentialsProvider(credentials.getAWSAccessKeyId, credentials.getAWSSecretKey)).withRegion(region)
@@ -33,24 +35,51 @@ object KinesisFirehose
 trait KinesisFirehose extends aws.AmazonKinesisFirehoseAsyncClient
 {
     
+    def sendBulkMessageToFirehoseAsync(payloads : Seq[String], deliveryStream : String) : java.util.concurrent.Future[PutRecordBatchResult] = 
+    {
+        val putRecordBatchRequest = createRecordBatchRequest(payloads, deliveryStream)
+        putRecordBatchAsync(putRecordBatchRequest)
+    }
+    
+    def sendBulkMessageToFirehose(payloads : Seq[String], deliveryStream : String) : PutRecordBatchResult = 
+    {
+        val putRecordBatchRequest = createRecordBatchRequest(payloads, deliveryStream)
+        putRecordBatch(putRecordBatchRequest)
+    }
+    
     def sendMessageToFirehoseAsync(payload : String, deliveryStream : String) : java.util.concurrent.Future[PutRecordResult] = 
     {
         val putRecordRequest = createRecordRequest(payload, deliveryStream)
-        this.putRecordAsync(putRecordRequest)
+        putRecordAsync(putRecordRequest)
     }
     
     def sendMessageToFirehose(payload : String, deliveryStream : String) : PutRecordResult =
     {
         val putRecordRequest = createRecordRequest(payload, deliveryStream)
-        this.putRecord(putRecordRequest)
+        putRecord(putRecordRequest)
+    }
+    
+    def createRecordBatchRequest(payloads : Seq[String], deliveryStream : String) : PutRecordBatchRequest =
+    {
+        val records = payloads.map(
+            payload =>
+            {
+                createRecord(payload)
+            }).asJava
+        new PutRecordBatchRequest().withDeliveryStreamName(deliveryStream).withRecords(records)
     }
     
     def createRecordRequest(payload : String, deliveryStream : String) : PutRecordRequest =
     {
-        val data = ByteBuffer.wrap(payload.getBytes(StandardCharsets.UTF_8))
-        val deliveryStreamRecord = new Record().withData(data)
+        val deliveryStreamRecord = createRecord(payload)
         new PutRecordRequest().withDeliveryStreamName(deliveryStream).withRecord(deliveryStreamRecord)
     }
+        
+    def createRecord(payload : String) : Record =
+    {
+        val data = ByteBuffer.wrap(payload.getBytes(StandardCharsets.UTF_8))
+        new Record().withData(data)
+    }   
 }
 
 class KinesisFirehoseAsyncClient(credentialsProvider : CredentialsProvider = CredentialsLoader.load())
