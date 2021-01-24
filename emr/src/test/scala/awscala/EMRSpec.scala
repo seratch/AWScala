c31b5837d940543615be6bb38d0f19d54c04cfbf
Rodolfo Echemendía Quintana <rechemendia@travelonux.com>
2021-01-24 12:23:33 -0300

    revert scala.jdk.CollectionConverters EMRSpec

--- emr/src/test/scala/awscala/EMRSpec.scala
+++ emr/src/test/scala/awscala/EMRSpec.scala
@@ -4,7 +4,7 @@ import org.slf4j._
 import org.scalatest._
 import awscala.emr._
 import awscala.ec2._
-import scala.jdk.CollectionConverters._
+import scala.collection.JavaConverters._
 
 
 class EMRSpec extends FlatSpec with Matchers with BeforeAndAfterAll {
