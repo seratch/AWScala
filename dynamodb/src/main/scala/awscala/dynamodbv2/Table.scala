a00af039eff59742f400afe249f4ee6447e48a82
Jakub Wszolek <Jakub.Wszolek@acxiom.com>
2021-02-09 12:37:21 +0100

    Changes after code review - getterNamesFromEntity added

--- dynamodb/src/main/scala/awscala/dynamodbv2/Table.scala
+++ dynamodb/src/main/scala/awscala/dynamodbv2/Table.scala
@@ -1,6 +1,8 @@
 package awscala.dynamodbv2
 
-import com.amazonaws.services.{ dynamodbv2 => aws }
+import java.lang.reflect.Modifier
+
+import com.amazonaws.services.{dynamodbv2 => aws}
 
 import scala.reflect.ClassTag
 
@@ -53,35 +55,50 @@ case class Table(
   def put(hashPK: Any, attributes: (String, Any)*)(implicit dynamoDB: DynamoDB): Unit = putItem(hashPK, attributes: _*)
   def put(hashPK: Any, rangePK: Any, attributes: (String, Any)*)(implicit dynamoDB: DynamoDB): Unit = putItem(hashPK, rangePK, attributes: _*)
 
-  def put(entity: Product)(implicit dynamoDB: DynamoDB) = {
-    val attrs = getAttrValuesToList(entity)
-    if(attrs("keys").exists(x => x._1 == "hashPK") && attrs("keys").exists(x => x._1 == "rangePK")) {
+  def put[E <: AnyRef](entity: E)(implicit dynamoDB: DynamoDB):Unit = {
+    val attrs = getAttrValuesToMap(entity)
+
+    if (attrs("keys").exists(x => x._1 == "hashPK") && attrs("keys").exists(x => x._1 == "rangePK")) {
       val hashPK = attrs("keys").find(f => f._1 == "hashPK").get._2
       val rangePK = attrs("keys").find(f => f._1 == "rangePK").get._2
       dynamoDB.put(this, hashPK, rangePK, attrs("attributes"): _*)
-    }
-    else if (attrs("keys").exists(x => x._1 == "hashPK")) {
+    } else if (attrs("keys").exists(x => x._1 == "hashPK")) {
       val hashPK = attrs("keys").find(f => f._1 == "hashPK").get._2
       dynamoDB.put(this, hashPK, attrs("attributes"): _*)
     }
   }
 
-  private def getAttrValuesToList(entity: Product) = {
-    val fields = entity.productElementNames.toList
-    val f1 = fields.map(f => f -> {
-      val fv = entity.getClass.getDeclaredField(f)
-      fv.setAccessible(true)
-      fv.get(entity)
-    })
+  private def getAttrValuesToMap(entity: Any):Map[String, List[(String, AnyRef)]] = {
+    val fields = getterNamesFromEntity(entity).map(getterName => {
+      val value = entity.getClass.getDeclaredMethod(getterName).invoke(entity)
+      getterName -> value
+    }).toList
 
-    val keys = f1.filter(f => f._1 == "hashPK" || f._1 == "rangePK")
-    val attrs = f1.filterNot(f => keys.exists(k => f._1 == k._1))
+    val keys = fields.filter(f => f._1 == "hashPK" || f._1 == "rangePK")
+    val attrs = fields.filterNot(f => keys.exists(k => f._1 == k._1))
 
-    if(!keys.exists(k => k._1 == "hashPK"))
+    if (!keys.exists(k => k._1 == "hashPK"))
       throw new Exception(s"Primary key is not defined for ${entity.getClass.getName}")
 
     Map("keys" -> keys, "attributes" -> attrs)
   }
+
+  private def getterNamesFromEntity(obj: Any): Seq[String] = {
+    val fieldNames = obj.getClass.getDeclaredFields
+      .filter(f => Modifier.isPrivate(f.getModifiers))
+      .filterNot(f => Modifier.isStatic(f.getModifiers))
+      .map(_.getName)
+
+    val methodNames = obj.getClass.getDeclaredMethods
+      .filter(m => Modifier.isPublic(m.getModifiers))
+      .filterNot(m => Modifier.isStatic(m.getModifiers))
+      .filterNot(m => m.getParameterTypes.size > 0)
+      .map(_.getName)
+
+    methodNames.filter(m => fieldNames.contains(m))
+  }
+
+
 
   def putItem(hashPK: Any, attributes: (String, Any)*)(implicit dynamoDB: DynamoDB): Unit = {
     dynamoDB.put(this, hashPK, attributes: _*)
